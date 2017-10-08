6e22551e3ebc8c68d88a6285fa6d4aae1575c491
bramandityo <freez.meinster@gmail.com>
2017-10-09 03:41:23 +0700

    perfection

--- apps/deploy/views/deploy_api.py
+++ apps/deploy/views/deploy_api.py
@@ -6,8 +6,10 @@ from deploy.decorators import basic_auth_required
 @basic_auth_required
 def deploy(request, code, key):
     try:
-        plan = DeployPlan.objects.get(code=code, auth_key=key)
+        plan = DeployPlan.objects.get(code=code,
+                                      auth_key=key,
+                                      is_active=True)
         plan.deploy()
-        return HttpResponse("deployed")
+        return HttpResponse("DEPLOYED")
     except ObjectDoesNotExist:
-        return HttpResponse("plan not exist")
+        return HttpResponse("NOT EXIST")
